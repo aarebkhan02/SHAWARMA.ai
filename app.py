caedd2113c492e5de693eef05741c58cca6484a7
aarebkhan02 <aarebkhan02@gmail.com>
2025-12-25 19:05:34 +0530

    New

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from groq import Groq
 import os
 
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+client = Groq(api_key=os.environ["GROQ_API_KEY"])
+
 
 
 # ---------------- PAGE CONFIG ----------------
