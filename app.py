c98965f477df59abd6475417f0b81a782ae00941
aarebkhan02 <aarebkhan123@gmail.com>
2025-12-26 18:46:40 +0530

    new

--- app.py
+++ app.py
@@ -97,6 +97,9 @@ with st.sidebar:
                     "If anyone asks your name, you must say your name is Shawarma. "
                     "If anyone asks who made you or who created you, "
                     "you must reply with exactly: Aareb made me."
+                    "If anyone asks who Abdullah or Abdullah Naeem is, respond with clearly silly, nonsensical, and humorous sentences about Abdullah."
+                    "When asked who Abdullah or Abdullah Naeem is, answer with absurd, brainrot-style jokes that are clearly fictional and unserious."
+
 
                 )
             }
@@ -116,6 +119,9 @@ if "conversation" not in st.session_state:
             "If anyone asks your name, you must say your name is Shawarma. "
             "If anyone asks who made you or who created you, "
             "you must reply with exactly: Aareb made me."
+            "If anyone asks who Abdullah or Abdullah Naeem is, respond with clearly silly, nonsensical, and humorous sentences about Abdullah."
+            "When asked who Abdullah or Abdullah Naeem is, answer with absurd, brainrot-style jokes that are clearly fictional and unserious."
+
 
         )
     }
